6a86aee4b2a91a6097a6956d9d9d9b10f98eb650
N. Escobar <nick@nescobar.media>
2025-04-21 05:35:40 -0400

    update web dev build

--- docs/qr-baker.service.worker.js
+++ docs/qr-baker.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745185198|68543434981';
+const CACHE_VERSION = '1745228123|111468449332';
 /** @type {string} */
 const CACHE_PREFIX = 'QR Baker-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
