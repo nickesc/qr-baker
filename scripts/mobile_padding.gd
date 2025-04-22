5c1654ac44ff6777ce9da4034efef2399e9f7626
N. Escobar <nick@nescobar.media>
2025-04-21 21:29:17 -0400

    remove mobile padding from web builds

--- scripts/mobile_padding.gd
+++ scripts/mobile_padding.gd
@@ -5,7 +5,7 @@ var mobile_style_box: StyleBox = preload("res://themes/mobile_stylebox.tres")
 
 func _ready() -> void:
     match OS.get_name():
-        "Android", "iOS", "Web":
+        "Android", "iOS":
             add_theme_stylebox_override("panel", mobile_style_box)
         _:
             add_theme_stylebox_override("panel", panel_style_box)
